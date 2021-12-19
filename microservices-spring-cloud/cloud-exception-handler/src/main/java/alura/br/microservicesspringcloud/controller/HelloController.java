cec4afe25ed18e706724dd6dd35c9460a89a665f
Aritana Noara <arinoara@yahoo.com.br>
2021-12-19 20:14:01 -0300

    first well done test for mongodb

--- microservices-spring-cloud/cloud-exception-handler/src/main/java/alura/br/microservicesspringcloud/controller/HelloController.java
+++ microservices-spring-cloud/cloud-exception-handler/src/main/java/alura/br/microservicesspringcloud/controller/HelloController.java
@@ -1,11 +1,14 @@
 package alura.br.microservicesspringcloud.controller;
 
+import alura.br.microservicesspringcloud.config.MongoDb;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import io.swagger.annotations.Api;
-import org.apache.logging.slf4j.SLF4JLogger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
-
+import org.bson.Document;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @Api
 @RestController
 @RequestMapping(value = "hello")
@@ -13,6 +16,12 @@ public class HelloController {
 
     @GetMapping(value="/{firstname}/{lastname}")
     public  String hello(@PathVariable("firstname")String firstname, @PathVariable("lastname")String lastname){
+
+        MongoDatabase database = MongoDb.client.getDatabase("exceptionHandlerDB");
+        MongoCollection<Document> alunos = database.getCollection("alunos");
+        Document aluno =  alunos.find().first();
+        System.out.println(aluno);
+
         return String.format("\"message\": \"Hello %s %s\" ", firstname, lastname);
     }
 }
